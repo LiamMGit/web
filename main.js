05773c41358cbd7528ec5956eb54698cbb66a86e
v1rtual-real1ty <64will64123@gmail.com>
2024-03-03 00:06:13 -0500

    updates
    updates

--- main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const content = document.getElementById('content');
-    content.classList.remove('hidden');
-    setTimeout(() => {
-        content.style.opacity = 1;
-        content.style.filter = "blur(0px)";
-    }, 100);
-  });
